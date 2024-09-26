0b668ecd0dd86992a0a7f66e5cdcb4cbf835d054
myau7 <mondi32323@gmail.com>
2024-09-26 12:12:09 +0500

    hfggh

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 import re as re
-data = {10: "Привет мир"}
+import sqlalchemy as db
+data = {"10" : "Привет мир"}
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    await update.message.reply_text(f'Привет {update.effective_user.first_name}.\nКоманды:\n /get "code" - получить сообщение по коду \n /add "code" - добавить сообщение по коду')
+    await update.message.reply_text(f'Привет {update.effective_user.first_name}.\nКоманды:\n /get "code" - получить сообщение по коду \n /add "code" - добавить сообщение по коду \n /getn получить новость по id  \n /addn добавить новость')
 
 async def get(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     raw = re.findall(r'\".*?\"', update.message.text)
@@ -23,6 +24,30 @@ async def add(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     message = re.findall(r'\".*?\"', update.message.text)[1].replace('"', ' ')
     data[code] = message
     await update.message.reply_text("ok")
+async def get_one_news(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    raw = update.message.text.split(" ")
+    if len(raw) != 2 : 
+        await update.message.reply_text("Проблема!")
+        return
+    id = int(raw[1])
+    engine = db.create_engine("mysql+pymysql://root@127.0.0.1/db?charset=utf8mb4")
+    conn = engine.connect()
+    query = db.text(f"SELECT * FROM news WHERE id = {id}")
+    news = conn.execute( query).fetchall()
+    await update.message.reply_text(str(news[0][0]) + "\n" + str(news[0][1]))  
+async def add_one_news(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    raw = update.message.text.split(" ")
+    if len(raw) != 3 : 
+        await update.message.reply_text("Проблема!")
+        return
+    name = str(raw[1])
+    description = str(raw[2])
+    engine = db.create_engine("mysql+pymysql://root@127.0.0.1/db?charset=utf8mb4")
+    conn = engine.connect()
+    query = db.text(f"INSERT INTO news (`name`,`description`) VALUES ('{name}','{description}');")
+    news = conn.execute(query)
+    conn.commit()
+    await update.message.reply_text("ok")  
 
 app = ApplicationBuilder().token("7699100126:AAElXwC_nMbwQaTzcXMo45AVRoQpIjCXkxM").build()
 
@@ -31,5 +56,9 @@ app.add_handler(CommandHandler("start",start))
 app.add_handler(CommandHandler("get",get))
 
 app.add_handler(CommandHandler("add",add))
+
+app.add_handler(CommandHandler("getn",get_one_news))
+
+app.add_handler(CommandHandler("addn",add_one_news))
 
 app.run_polling()
